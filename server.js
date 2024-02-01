43940f1347e44d857ecb8930cec78f3e852f775a
Andrew <a.nikanorov@gmail.com>
2024-02-01 14:43:07 +0100

    change port

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 /** @format */
 
-import { set, connect } from 'mongoose';
+import mongoose from 'mongoose';
+
 import app from './app.js';
 
 const { DB_HOST, PORT = 4000 } = process.env;
-set('strictQuery', true);
 
-connect(DB_HOST)
-	.then(() => app(PORT))
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server running. Use our API on port: ${PORT}`);
+		});
+	})
 	.catch(error => {
 		console.log(error.message);
 		process.exit(1);
