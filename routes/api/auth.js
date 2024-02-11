06f3bf64da2b71d12093a43e03aa317ad5171d84
Pavlo2005 <savyukpavlo8@gmail.com>
2024-02-11 11:46:52 +0100

    conflict resolution

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 
 import { validateBody, authenticate, isEmptyBody } from '../../middlewares/index.js';
 import { registerSchema, loginSchema, registerSchemaForGoogle } from '../../models/index.js';
-import { register, login, logout, getCurrent, registerForGoogle,googleAuth, googleRedirect } from '../../controllers/auth/index.js';
+import { register, login, logout, getCurrent, registerForGoogle } from '../../controllers/auth/index.js';
 import { ctrlWrapper } from '../../utils/index.js';
 
 const authRouter = Router();
@@ -23,11 +23,5 @@ authRouter.post('/logout', authenticate, ctrlWrapper(logout));
 
 // Refresh
 authRouter.get('/current', authenticate, ctrlWrapper(getCurrent));
-
-//Google
-authRouter.get('/google', ctrlWrapper(googleAuth));
-
-//Google-Redirect
-authRouter.get('/google-redirect', ctrlWrapper(googleRedirect));
 
 export default authRouter;
