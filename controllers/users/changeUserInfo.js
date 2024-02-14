af4436a05c58be51cf0db72b1530743c1e479337
Andrew <a.nikanorov@gmail.com>
2024-02-14 18:31:17 +0100

    fix changeUser profile

--- controllers/users/changeUserInfo.js
+++ controllers/users/changeUserInfo.js
@@ -1,61 +1,56 @@
-import { hash } from "bcrypt";
-import jwt from "jsonwebtoken";
-import { User, Token } from "../../models/index.js";
-import { cloudinary } from "../../utils/index.js";
-import Jimp from "jimp";
-import fs from "fs/promises";
+/** @format */
+
+import { hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User, Token } from '../../models/index.js';
+import { cloudinary } from '../../utils/index.js';
+import Jimp from 'jimp';
+import fs from 'fs/promises';
 
 const { SECRET_KEY } = process.env;
 
 export async function updateUserInfo(req, res) {
-  const { _id } = req.user;
-  const { email, name, password = "" } = req.body;
-  const user = await User.findById(_id);
+	const { _id } = req.user;
+	const { name, password = '' } = req.body;
+	const user = await User.findById(_id);
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  
-  let avatarURL = user.avatarURL;
+	if (!user) {
+		return res.status(404).json({ message: 'User not found' });
+	}
 
-  if (req.file) {
-    const image = await Jimp.read(req.file.path);
-    await image.resize(68, 68);
-    await image.writeAsync(req.file.path);
+	let avatarURL = user.avatarURL;
 
-    // Загружаем аватар в Cloudinary и получаем URL
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "avatars",
-    });
-    avatarURL = result.url;
-    await fs.unlink(req.file.path);
-  }
+	if (req.file) {
+		const image = await Jimp.read(req.file.path);
+		image.resize(68, 68);
+		await image.writeAsync(req.file.path);
 
-  let token = "";
+		const result = await cloudinary.uploader.upload(req.file.path, {
+			folder: 'avatars',
+		});
+		avatarURL = result.url;
+		await fs.unlink(req.file.path);
+	}
 
-  if (password) {
-    const payload = { id: user._id };
-    token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
-    const newToken = new Token({ userId: user._id, token });
-    await newToken.save();
-  }
-  const hashPassword = await hash(password, 10);
+	const hashPassword = password ? await hash(password, 10) : null;
 
-  await User.findByIdAndUpdate(_id, {
-    email,
-    name,
-    avatarURL,
-    password: hashPassword,
-  });
+	const updateFields = {};
 
-  res.json({
-    token: token,
-    user: {
-      _id,
-      name,
-      email,
-      avatarURL,
-      thema: user.thema,
-    },
-  });
+	if (name) updateFields.name = name;
+	if (avatarURL) updateFields.avatarURL = avatarURL;
+	if (hashPassword) updateFields.password = hashPassword;
+
+	const updatedUser = await User.findByIdAndUpdate(_id, updateFields, { new: true });
+
+	const resp = {};
+	if (password) {
+		const payload = { id: updatedUser._id };
+		const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
+		const newToken = new Token({ userId: updatedUser._id, token });
+		await newToken.save();
+		resp.token = token;
+	}
+	resp.user = updatedUser;
+
+	res.json(resp);
 }
