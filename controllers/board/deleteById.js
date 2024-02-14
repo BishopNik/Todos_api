aa672292a9c003cbe9b04f0166a0eec8dd598bde
Andrew <a.nikanorov@gmail.com>
2024-02-14 12:46:35 +0100

    fix del board, column

--- controllers/board/deleteById.js
+++ controllers/board/deleteById.js
@@ -7,8 +7,12 @@ export const deleteById = async (req, res, next) => {
 	const { _id: owner } = req.user;
 	const { boardId } = req.params;
 	const resColumn = await Columns.find({ boardId, owner });
-	resColumn.map(async item => await Cards.findByIdAndDelete({ columnId: item._id, owner }));
-	await Columns.findByIdAndDelete({ boardId, owner });
+	await Promise.all(
+		resColumn.map(async item => {
+			await Cards.deleteMany({ columnId: item._id, owner });
+		})
+	);
+	await Columns.deleteMany({ boardId, owner });
 	const result = await Boards.findByIdAndDelete({ _id: boardId, owner });
 	if (!result) {
 		throw httpError(404, `Boarder with id=${boardId} not found`);
--- controllers/column/deleteById.js
+++ controllers/column/deleteById.js
@@ -6,7 +6,7 @@ import { httpError } from '../../utils/httpError.js';
 export const deleteById = async (req, res, next) => {
 	const { _id: owner } = req.user;
 	const { columnId } = req.params;
-	await Cards.findByIdAndDelete({ columnId, owner });
+	await Cards.deleteMany({ columnId: columnId, owner: owner });
 	const result = await Columns.findByIdAndDelete({ _id: columnId, owner });
 	if (!result) {
 		throw httpError(404, `Column with id=${columnId} not found`);
