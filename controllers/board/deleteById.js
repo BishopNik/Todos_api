d9bc3a7db928a06d3966a9acc67a43bd703b8d7a
Andrew <a.nikanorov@gmail.com>
2024-02-14 10:52:21 +0100

    fix Delete Boards, Columns

--- controllers/board/deleteById.js
+++ controllers/board/deleteById.js
@@ -1,16 +1,21 @@
-import { Boards } from "../../models/index.js";
-import { httpError } from "../../utils/index.js";
+/** @format */
+
+import { Boards, Columns, Cards } from '../../models/index.js';
+import { httpError } from '../../utils/index.js';
 
 export const deleteById = async (req, res, next) => {
-    const {_id: owner} = req.user;
-    const { boardId } = req.params;
-    const result = await Boards.findByIdAndDelete({_id: boardId, owner});
-    if (!result) {
-        throw httpError(404, `Boarder with id=${boardId} not found`);
-    }
-    
-    res.json({
-        name: result.name,
-        _id: result._id
-    })
-}
+	const { _id: owner } = req.user;
+	const { boardId } = req.params;
+	const res = await Columns.find({ boardId, owner });
+	res.map(async item => await Cards.findByIdAndDelete({ columnId: item._id, owner }));
+	await Columns.findByIdAndDelete({ boardId, owner });
+	const result = await Boards.findByIdAndDelete({ _id: boardId, owner });
+	if (!result) {
+		throw httpError(404, `Boarder with id=${boardId} not found`);
+	}
+
+	res.json({
+		name: result.name,
+		_id: result._id,
+	});
+};
--- controllers/column/deleteById.js
+++ controllers/column/deleteById.js
@@ -1,16 +1,19 @@
-import { Columns } from "../../models/index.js";
-import { httpError } from "../../utils/httpError.js";
+/** @format */
+
+import { Columns, Cards } from '../../models/index.js';
+import { httpError } from '../../utils/httpError.js';
 
 export const deleteById = async (req, res, next) => {
-    const {_id: owner} = req.user;
-    const { columnId } = req.params;
-    const result = await Columns.findByIdAndDelete({_id: columnId, owner});
-    if (!result) {
-        throw httpError(404, `Column with id=${columnId} not found`);
-    }
+	const { _id: owner } = req.user;
+	const { columnId } = req.params;
+	await Cards.findByIdAndDelete({ columnId, owner });
+	const result = await Columns.findByIdAndDelete({ _id: columnId, owner });
+	if (!result) {
+		throw httpError(404, `Column with id=${columnId} not found`);
+	}
 
-    res.json({
-        name: result.name,
-        _id: result._id
-    })
-}
+	res.json({
+		name: result.name,
+		_id: result._id,
+	});
+};
